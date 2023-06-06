e80f87395a297c3ad096616cb8cca1da186565ca
weslleylima10 <weslleyjhonesbr@gmail.com>
2023-06-06 08:29:32 -0300

    fizemos um balao em java,foi incrivel

--- src/Balao.java
+++ src/Balao.java
@@ -1,2 +1,17 @@
 public class Balao {
+    //Atributos
+    int id;
+    String registro;
+    String cor;
+    String tipo;
+    int capacidade;
+    double altura;
+    String porte;
+    double peso;
+
+    // Métodos
+    public String voar (){
+        return "voando";
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+            // Construir balões
+            Balao b1 = new Balao();
+            String resultado = b1.voar();
+            System.out.println(resultado);
     }
-}
